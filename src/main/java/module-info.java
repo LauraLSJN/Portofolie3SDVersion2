ec33582097a4307842b1c2a3e8c37df738f17518
LauraLSJN <lsjn@ruc.dk>
2022-11-08 10:12:04 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.portofolie3sdversion2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.portofolie3sdversion2 to javafx.fxml;
+    exports com.example.portofolie3sdversion2;
+}
